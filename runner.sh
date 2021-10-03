a89964a982607bc4a8ef2966f241faea92e0532f
mirisbowring <mirisbowring@outlook.de>
2021-10-03 03:56:05 +0200

    Fix DNS lookup failures and expose WKHTMLTOPDF_OPTIONS  (#12)

--- runner.sh
+++ runner.sh
@@ -2,4 +2,16 @@
 
 echo ${INTER_RUN_INTERVAL}
 
+#add hosts to /etc/hosts to prevent HostNotFoundErrors
+if [[ ! -z "${HOSTS}" ]]; then
+  oIFS=${IFS}
+  IFS=";"
+  declare -a newhosts=($HOSTS)
+  IFS="$oIFS"
+  unset oIFS
+  for i in "${newhosts[@]}"; do
+    echo "$i" >> /etc/hosts
+  done
+fi
+
 while poetry run /app/main.py; do sleep ${INTER_RUN_INTERVAL}; done
